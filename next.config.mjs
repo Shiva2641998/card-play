a9b02c2a55275fd41c633b774f38f6ad2d8c9461
shivam <shiva2641998@gmail.com>
2024-11-13 11:38:32 +0530

    fix all 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     // Enable React strict mode
+  reactStrictMode: true,
+
+  // Customize Webpack configuration
+  webpack: (config, { isServer }) => {
+    // Example: Handle server-only or client-only code
+    if (!isServer) {
+      // Mock Node.js-specific modules on the client side
+      config.resolve.fallback = {
+        fs: false, // Ignore file system module on the client side
+        path: false, // Ignore path module on the client side
+      };
+    }
+
+    // Add other custom rules or loaders if needed
+    return config;
+  },
+};
 
 export default nextConfig;
